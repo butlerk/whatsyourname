877804c053d46c69a6adc0210aa04718a42c341b
Kelly Butler <38035824+butlerk@users.noreply.github.com>
2023-03-15 22:18:46 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+print("Hello and welcome to my program")
+print("Today I will ask you a few questions")
+
+name = input("What is your name? ")
+print(f"Cool! I like the name {name}")
+
+age = input("What is your age? ")
+print("Wow that's young")
+
+fav_food = input("What is your favourite food? ")
+song = input("What is your favourite song? ")
+colour = input("What is a colour? ")
+adress = input("where do you live? " )
+car_type = input("what type of car do you have? ")
+footy_team = input ("which footy team do you go for")
+
+print(f"Hello {name}, I really like {fav_food} and the colour {colour} too! \n You are very young at age {age}. Let's sing {song} together! I'll come visit you in {adress}. your {car_type} is preety slow compaired to my lambo.lets go watch the {footy_team} together")
